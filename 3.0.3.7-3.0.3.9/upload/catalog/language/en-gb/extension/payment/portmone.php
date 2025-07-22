8cc831cf4402be90a94d22d5be7150c09dfde1c8
ochertok <ochertok@portmone.me>
2025-07-22 14:07:49 +0300

    SD-19399: Додав можливість обробки нотифікацій для версій 3.0.3.7-3.0.3.9

---
+++ 3.0.3.7-3.0.3.9/upload/catalog/language/en-gb/extension/payment/portmone.php
@@ -0,0 +1,16 @@
+<?php
+// Text
+$_['text_title']                        = 'Portmone';
+$_['button_pay']                        = 'Payment for order through Portmone';
+$_['img_portmone']                      = '<a target="_blank" href="https://www.portmone.com.ua/"><img width="200" src="admin/view/image/payment/portmone.svg" title="Portmone.com"></a>';
+$_['heading_title_failure']             = 'Order payment failed';
+$_['heading_title_failure_success']     = 'Payment for the order was successful';
+$_['number_pay']                        = 'Your order number';
+$_['error_order_in_portmone']           = 'There is no payment in the Portmone system, it is returned or created incorrectly';
+$_['thankyou_text']                     = 'Thank you for purchasing!';
+$_['error_orderid']                     = 'Error during payment. Contact the online store for order verification';
+$_['error_auth']                        = 'Authorisation Error. Invalid login or password entered';
+$_['error_merchant']                    = 'Error during payment. Online store data is incorrect';
+$_['order_rejected']                    = 'Error during payment. Check your card details and try to pay again!';
+$_['repeated_payment']                  = 'The status of this order is Paid, it cannot be paid again';
+$_['notification_error']                = 'Error while processing notification';
